8295f18e2980279dac699110d4c7f304b58d084b
fazli <khista.rahman@gmail.com>
2020-02-20 01:42:49 -0800

    add more modification to files

--- js/1s.js
+++ js/1s.js
@@ -2,5 +2,7 @@ let paragraph = document.createElement('p');
 paragraph.innerHTML="Welcome to GuardWatch";
 paragraph.style.fontWeight='10em';
 paragraph.style.color='green';
+let firstHeaderImage=document.getElementById('headerImage');
+firstHeaderImage.setAttribute('id','headerImage');
 document.body.appendChild(paragraph);
 
