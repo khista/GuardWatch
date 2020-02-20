54f0f14978b3b81a81a3d5ab3a7880c66368bed5
fazli <khista.rahman@gmail.com>
2020-02-20 01:09:35 -0800

    test js and css file

---
+++ js/1s.js
@@ -0,0 +1,6 @@
+let paragraph = document.createElement('p');
+paragraph.innerHTML="Welcome to GuardWatch";
+paragraph.style.fontWeight='10em';
+paragraph.style.color='green';
+document.body.appendChild(paragraph);
+
