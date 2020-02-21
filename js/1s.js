a1337004a0cca06e2c39cb99abd935f607978dfd
fazli <khista.rahman@gmail.com>
2020-02-21 02:57:51 -0800

    add more to project

--- js/1s.js
+++ js/1s.js
@@ -1,8 +1,32 @@
+
+
+
+let nav = document.querySelector('nav');
+nav.setAttribute('id','headNav');
+
+/* //add paragraph
 let paragraph = document.createElement('p');
 paragraph.innerHTML="Welcome to GuardWatch";
 paragraph.style.fontWeight='10em';
 paragraph.style.color='green';
-let firstHeaderImage=document.getElementById('headerImage');
-firstHeaderImage.setAttribute('id','headerImage');
 document.body.appendChild(paragraph);
+
+
+// div for menu
+let divMenuBar = document.querySelector('div');
+divMenuBar.style.background="red";
+divMenuBar.style.width='300px;';
+divMenuBar.style.height='250px';
+  */
+
+
+/* //header image property 
+let headimage = document.querySelector('img');
+headimage.style.width="100%";
+headimage.style.height="690px";
+headimage.style.zIndex='0';
+ 
+headimage.setAttribute('id','headerimage'); */
+
+
 
